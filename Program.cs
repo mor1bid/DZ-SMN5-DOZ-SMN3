e4b721447431f85c0e384cba9440a436aa78e799
De <spark49942@gmail.com>
2022-08-05 21:44:32 +0300

    Z4-2

--- Program.cs
+++ Program.cs
@@ -86,7 +86,7 @@ Console.Write("\b\b" + "]");
 Console.WriteLine();
 i = 0;
 int j = size;
-while(i != j) 
+while(i > size) 
 {
 Console.WriteLine("Произведение " + $"{ray[i]}" + " * " + $"{ray[j]}" + " = " + $"{ray[i]*ray[j]}");
 j--;
