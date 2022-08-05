eb0d5ea72e9d9a94a77f597af6c8acad56d3b7e7
De <spark49942@gmail.com>
2022-08-05 20:48:20 +0300

    Z2

--- Program.cs
+++ Program.cs
@@ -9,7 +9,32 @@ Z1();
 Console.WriteLine();
 void Z2() 
 {
-
+int [] ray = new int [5];
+int i = 0;
+Console.Write("Был [");
+while (i<ray.Length) 
+{
+int rayn = new Random().Next(0, 11);
+ray[i] = rayn;
+Console.Write(($"{ray[i]}" + ", "));
+i++;
+}
+i = 0;
+Console.Write("\b\b" + "]");
+Console.WriteLine();
+Console.Write("Стал: [");
+//int j = i + 1;
+while (i<ray.Length) 
+{
+    int min = i;
+    //while (j<ray.Length) 
+    //{
+        if (ray[i] < 0) ray[i] = Math.Abs(ray[i]);
+        else if (ray[i] > 0) ray[i] *= -1;
+    //}
+    Console.Write(ray[i] + ", ");
+    i++;
+}
 
 }
 Console.WriteLine();
