a5ade1a20a9e137412ac32ff0a2e2d74e61e740f
De <spark49942@gmail.com>
2022-08-05 20:23:21 +0300

    Z3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Z1() 
+{
+
+
+}
+Console.WriteLine();
+Z1();
+
+Console.WriteLine();
+void Z2() 
+{
+
+
+}
+Console.WriteLine();
+Z2();
+
+Console.WriteLine();
+void Z3() 
+{
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine()), 
+    i = 0;
+int [] ray = new int [5];
+while (i<ray.Length) 
+{
+int rayn = new Random().Next(0, 2000000001);
+ray[i] = rayn;
+if (ray[i] == num) Console.WriteLine("Число " + num + " есть в массиве " + ($"{ray[i]} + ", ""));
+i++;
+}
+
+
+
+}
+Console.WriteLine();
+Z3();
