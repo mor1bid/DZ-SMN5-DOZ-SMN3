8bc299e4b7dc9e074f98e5124e2bdb1b4b433880
De <spark49942@gmail.com>
2022-08-07 01:19:36 +0300

    DZ1-FRay

--- Program.cs
+++ Program.cs
@@ -1,20 +1,38 @@
 ﻿void DZ1() 
 {
 int [] rayh = new int [4];
-int co = 0;
+int i = 0, 
+    size = rayh.Length, 
+    eco = 0, 
+    uco = 0;
 Console.Write("В массиве [");
-for (int i = 0, size = rayh.Length; i < size; i++)
+void FRay() 
 {
-int num = new Random().Next(100, 1000);
-rayh[i] = num;
-Console.Write(rayh[i] + ", ");
-if (rayh[i] % 2 == 0) co++;
+    for (int i = 0; i < size; i++)
+    {
+        int num = new Random().Next(100, 1000);
+        rayh[i] = num;
+        Console.Write(rayh[i] + ", ");
+        //if (rayh[i] % 2 == 0) eco++;
+        //else uco++;
+    }
 }
-Console.Write("\b\b" + "] - " + co + " чётных чисел" );
+FRay();
+i = 0;
+    while (i<size) 
+    {
+        if (rayh[i] % 2 == 0) eco++;
+        else uco++;
+        i++;
+    }
+Console.Write("\b\b" + "] - " + eco + " чётных чисел и " + uco + " нечётных." );
+Console.WriteLine();
 }
 
+Console.WriteLine();
 void DZ2() 
 {
+int [] ray = new int [4];
 
 }
 DZ1();
