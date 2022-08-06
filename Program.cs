d84a83c6a76f7595437c0bc85db168a74580d5cb
De <spark49942@gmail.com>
2022-08-07 00:47:49 +0300

    DZ

--- Program.cs
+++ Program.cs
@@ -1,130 +1,21 @@
-﻿void Z1() 
+﻿void DZ1() 
 {
-int [] ar = new int [12];
-
-}
-
-//void FillArray(int[] ar, int starttNumber = -10, int finishNumber = 10)
-Console.WriteLine();
-Z1();
-
-Console.WriteLine();
-void Z2() 
-{
-int [] ray = new int [5];
-int i = 0;
-Console.Write("Был: [");
-while (i<ray.Length) 
-{
-int rayn = new Random().Next(-11, 12);
-ray[i] = rayn;
-Console.Write(($"{ray[i]}" + ", "));
-i++;
-}
-i = 0;
-Console.Write("\b\b" + "]");
-Console.WriteLine();
-Console.Write("Стал: [");
-while (i<ray.Length) 
-{
-        if (ray[i] < 0) ray[i] = Math.Abs(ray[i]);
-        else if (ray[i] > 0) ray[i] *= -1;
-    Console.Write(ray[i] + ", ");
-    i++;
-}
-Console.Write("\b\b" + "]");
-}
-Console.WriteLine();
-Z2();
-
-Console.WriteLine();
-void Z3() 
-{
-Console.WriteLine("Введите число:");
-int num = Convert.ToInt32(Console.ReadLine()), 
-    i = 0;
-int [] ray = new int [5];
-Console.Write("В массиве [");
-while (i<ray.Length) 
-{
-int rayn = new Random().Next(0, 11);
-ray[i] = rayn;
-Console.Write(($"{ray[i]}" + ", "));
-i++;
-}
-i = 0;
-while (i<ray.Length) 
-{
-if (ray[i] == num) 
-{ 
-    Console.Write("\b\b" + "]");
-    Console.Write(" есть число " + num); 
-    return; 
-    }
-else i++;
-}
-Console.Write("\b\b" + "]");
-Console.WriteLine(" нет числа " + num); 
-}
-Console.WriteLine();
-Z3();
-
- Console.WriteLine();
- void Z4() 
- {
- int [] ray = new int [5];
- int i = 0,
- size = ray.Length;
- Console.Write("Массив [");
- while (i<ray.Length) 
- {
- int rayn = new Random().Next(0, 11);
- ray[i] = rayn;
- Console.Write(($"{ray[i]}" + ", "));
- i++;
- }
- i = 0;
- Console.Write("\b\b" + "]");
- Console.WriteLine();
- int mi = size - 1, mid = size/2, res = ray[i] * ray[mi-i];
- while (i < mid)
- {
-     res = ray[i] * ray[mi-i];
-     Console.WriteLine("Произведение " + ray[i] + " * " + ray[mi-i] + " = " + res);
-     i++;
-}
-if (size % 2 != 0) 
-{
-    res = ray[i] * ray[mi-i]; 
-    Console.WriteLine("Произведение " + ray[i] + " * " + ray[mi-i] + " = " + res);
-}
-}
-Console.WriteLine();
-Z4();
-
-void Z5() 
-{
-int [] ray = new int [10];
-int i = 0,
-size = ray.Length;
-Console.Write("В массиве [");
-while (i<ray.Length) 
-{
-int rayn = new Random().Next(0, 151);
-ray[i] = rayn;
-Console.Write(($"{ray[i]}" + ", "));
-i++;
-}
-i = 0;
+int [] rayh = new int [4];
 int co = 0;
-Console.Write("\b\b" + "]");
-Console.WriteLine();
-while(i < size) 
+Console.Write("В массиве [");
+for (int i = 0, size = rayh.Length; i < size; i++)
 {
-    if (ray[i] >= 10 && ray[i] <= 99) co++;
-    i++;
+int num = new Random().Next(100, 1000);
+rayh[i] = num;
+Console.WriteLine(rayh[i] + ", ");
+if (rayh[i] % 2 == 0) co++;
 }
-Console.Write(" - " + co + " элемента(ов) в данном промежутке");
+Console.Write("/b/b" + "] - " + co + "чётных чисел" );
 }
-Console.WriteLine();
-Z5();
+
+void DZ2() 
+{
+
+}
+DZ1();
+DZ2();
