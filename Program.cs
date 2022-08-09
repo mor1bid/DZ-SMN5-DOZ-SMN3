1f525dc73340f0f1d7f225dd855081044e9c3063
De <spark49942@gmail.com>
2022-08-09 03:59:07 +0300

    SMN5DZ+SMN3DOZ-DOZ71

--- Program.cs
+++ Program.cs
@@ -88,7 +88,7 @@ i = 0;
 int yi = 0,
     mi = 1;
 Console.WriteLine("В " + yi + " году:");
-while (yi<year) 
+while (yi-1<year) 
     {
     int temp = mass [i], min = 0, plu = 0;
         if (temp>plu) plu = temp;
